6fddefddea98bc4612be06a7789d18193ce45613
Nowell Gata <nowell.gata@gmail.com>
2019-04-30 14:40:11 -0500

    added index.js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+<script type="text/javascript" src="index.js">
+
+var name = prompt('What is your name?');
+
+var checkName = function(){
+    if(name === ''){
+        name = prompt('What is your name, for real this time?');
+
+        checkName(); // repeats until a name exists
+    }
+};
+
+checkName(); // kicks off the name-checking the first time
+
+alert('Hello ' + name);
+
+</script>

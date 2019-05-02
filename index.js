3e13d6901e9739faa7dd89de2a375083ae704328
Nowell Gata <nowell.gata@gmail.com>
2019-05-01 19:26:12 -0500

    refactored to check for falsiness

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-<script>
 
-  let name = prompt("What is your name?");
-  function checkName(name) {
-  if (name === "") {
-        name = prompt("Really, what is your name?");
+
+let name = prompt('What is your name?');
+
+function checkName(name){
+    // if name is falsey
+    if(!name){
+        name = prompt('Really, what is your name?');
         name = checkName(name);
-      }
-  return name;
     }
-  name = checkName(name);
-  alert(`Hello ${name}! What is for dinner??`);
 
-</script>
+    return name;
+}
+name = checkName(name);
+alert(`Hello ${name}! What is for dinner??`);
+
+document.querySelector('h1').textContent = 'Hello ' + name;
+

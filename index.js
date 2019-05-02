942f6df16c68502f2a1d38c2cc87d11fd0c876b9
Nowell Gata <nowell.gata@gmail.com>
2019-05-01 21:22:33 -0500

    Added modularized parcels into a JavaScript file

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ import Footer from './Components/Footer';
 
 console.log(Navigation, ContentOne, ContentTwo, Footer);
 
+// Use innerHTML property as a GETTER
+let initialHTML = document.body.innerHTML;
+
+// Use innerHTML property as a SETTER
+document.body.innerHTML = `
+  ${Navigation}
+  ${ContentOne}
+  ${ContentTwo}
+  ${Footer}
+  ${initialHTML}
+  `;

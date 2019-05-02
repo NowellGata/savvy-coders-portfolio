4713dd19c03c47ce700fc220d7c378331042976e
Nowell Gata <nowell.gata@gmail.com>
2019-05-01 20:34:57 -0500

    Worked with arrays

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
+// established an empty array in the Global Scope
+let nameArr = [];
 
+let nameArr[0] = prompt('What is your FIRST name?');
+let nameArr[1] = prompt('what is your LAST name??');
+// adds elements to the end of an array
 
-let name = prompt('What is your name?');
+// nameArr.push(fname);
+// nameArr.push(lname);
+// name is an Array of fname and lname
 
 function checkName(name){
     // if name is falsey
-    if(!name){
-        name = prompt('Really, what is your name?');
+    // developer's note: we are requiring both pieces of information even if only 1 is blank
+
+  if(!name[0] || !name[1]){
+
+        name[0] = prompt('Really, what is your FIRST name?');
+        name[1] = prompt('Really, what is your LAST name??');
+
         name = checkName(name);
     }
 
     return name;
 }
-name = checkName(name);
+
+
+nameArr = checkName(nameArr);
+
 alert(`Hello ${name}! What is for dinner??`);
 
-document.querySelector('#greeting').innerHTML = `<p>${name}`;
+document.querySelector('#greeting').innerHTML = `<p>${nameArr[0]} ${nameArr[1]}`;
 
 

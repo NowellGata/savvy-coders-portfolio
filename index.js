3da565d4a44411099535faac6a072692dfebda04
Nowell Gata <nowell.gata@gmail.com>
2019-05-07 16:09:41 -0500

    placing back tick enclosure around template literals aka interpolations

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ import Main from './components/Main';
 // State is an Object Literal that will hold information that functional components to properly render
 
 const state = {};
+
 state.title = 'Some Title';
 
 // Use innerHTML property as a SETTER
 document.querySelector('#root').innerHTML =
-//In each of these, we are invoking our fxns, and the 'return' is the resulting HTML.'
-  ${Navigation(state)}
+// In each of these, we are invoking our fxns, and the 'return' is the resulting HTML.
+  `${Navigation(state)}
   ${Header(state)}
   ${Main(state)}
     `;

162c6eb31efd56eb138901b1aeca5c0c26902331
Nowell Gata <nowell.gata@gmail.com>
2019-05-08 18:55:27 -0500

    Use render fxn

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ import Header from './components/Header';
 import Main from './components/Main';
 
 const state = {};
-
+const home = {
+  'title': "Domo Arigato Mr. Roboto!";
+}
 state.title = 'Nowell Gata';
 
 // Use innerHTML property as a SETTER
-document.querySelector('#root').innerHTML =
+const root = document.querySelector('#root');
 // In each of these, we are invoking our fxns, and the 'return' is the resulting HTML.
-  `${Navigation(state)}
+
+
+// Render receives an ARGUMENT as a NAMED PARAMETER, 'State'
+function render(state) {
+  root.innerHTML = `
+  ${Navigation(state)}
   ${Header(state)}
   ${Main(state)}
     `;
 
+}
+
+render(home);

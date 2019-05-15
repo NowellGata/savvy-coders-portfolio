c339de503e89ab9cba64bbb2a485d6161410be5d
Nowell Gata <nowell.gata@gmail.com>
2019-05-14 19:49:43 -0500

    Add import for footer on index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Navigation from './components/Navigation';
 import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/footer';
 // Nest states
 
 state.title = 'Nowell Gata';

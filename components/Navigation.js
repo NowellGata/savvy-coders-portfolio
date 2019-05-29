f7b50f031147185539382c1f18fb38eaa2f115c2
Nowell Gata <nowell.gata@gmail.com>
2019-05-29 18:25:09 -0500

    Refactor Navigation.js

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,36 +2,72 @@ function buildIconHTML(link){
     if(link.icon){
         return `<span class="${link.icon}"></span>`;
     }
+
+    return '';
 }
 
 function buildNavHTML(stateLinks){
-    let linksHTML = '';
-
-    stateLinks.forEach((link) => {
-        linksHTML += `
-        <li>
-          <a href="/${link.text.toLowerCase()}" data-navigo>
-          ${buildIconHTML(link)} ${link.text}
-          </a>
-        </li>`;
-    });
-
-    return linksHTML;
+    return stateLinks
+        .map(
+            (link) =>
+                `<li><a href="/${link.text.toLowerCase()}" data-navigo>${buildIconHTML(
+                    link
+                )}${link.text}</a></li>`
+        )
+        .join(' ');
 }
 
-export default (state) => {
-    console.log('Navigation has this state', state);
-
-    return `
-  <nav class= "navbar">
+export default (state) => `<nav>
     <ul class="flex">
       ${buildNavHTML(state.links.primary)}
       <li>
         Portfolio
-        <ul class = "dropdown">
-           ${buildNavHTML(state.links.dropdown)}
+        <ul id="dropdown">
+          ${buildNavHTML(state.links.dropdown)}
         </ul>
       </li>
     </ul>
-</nav>`;
-};
+  </nav>`;
+
+// function buildIconHTML(link){
+//     if(link.icon){
+//         return `<span class="${link.icon}"></span>`;
+//     }
+// }
+
+// function buildNavHTML(stateLinks) {
+//   return stateLinks.map(
+//     (link) =>
+//     `<li><a href="/${link.text.toLowerCase()}" data-navigo>  ${buildIconHTML(link)} ${link.text}
+//   </a>
+// </li>`
+// )
+// ;
+
+// let linksHTML = '';
+//     // Remove all the forEach and replace with MAP
+//     stateLinks.map((link) =>
+//     stateLinks.forEach((link) => {
+//         linksHTML += `
+
+//     });
+
+//     return linksHTML;
+// }
+
+// export default (state) => {
+//     console.log('Navigation has this state', state);
+
+//     return `
+//   <nav class= "navbar">
+//     <ul class="flex">
+//       ${buildNavHTML(state.links.primary)}
+//       <li>
+//         Portfolio
+//         <ul class = "dropdown">
+//            ${buildNavHTML(state.links.dropdown)}
+//         </ul>
+//       </li>
+//     </ul>
+// </nav>`;
+// };

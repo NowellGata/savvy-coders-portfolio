73d4eda0250a79d87339b6fec4e3828d7e527532
Nowell Gata <nowell.gata@gmail.com>
2019-06-03 19:53:31 -0500

    Update Blog page with live post data

--- components/pages/Blog.js
+++ components/pages/Blog.js
@@ -1,3 +1,16 @@
+function buildPostsHTML(posts){
+    // console.log('I received posts', posts);
+    return posts.map((post) => `<article>
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+        </article>`
+    );
+}
+
+
 export default function(state){
-    return `<h1>Hello from the blog! ${state.title}</h1>`;
+    return `
+    <h1>Hello from the Blog! ${state.title}</h1>
+    ${buildPostsHTML(state.posts)};
+    `;
 }

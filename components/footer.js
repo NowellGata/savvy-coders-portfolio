4558ae6bbfe1c19643d2011810985eaf3368f283
Nowell Gata <nowell.gata@gmail.com>
2019-06-06 15:24:43 -0500

    Clean up footer

--- components/footer.js
+++ components/footer.js
@@ -1,22 +1,37 @@
 export default (state) =>
 
-    `<footer class="container flex">
-      &copy; 2019 Nowell Gata
-      <ul>
-        <li>
-          <a href="https://github.com/NowellGata" target="_blank">
-            <span class="fab fa-github"></span>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/nowellgata/" target="_blank">
-            <span class="fab fa-linkedin-in"></span>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/nolerama" target="_blank">
-            <span class="fab fa-instagram"></span>
-          </a>
-        </li>
-      </ul>
-    </footer>`;
+    `<footer>
+  &copy; 2019 Nowell Gata
+  <a href="https://github.com/NowellGata" target="blank">
+    <i class="fab fa-linkedin"></i>
+  </a>
+  <a href=<a href="https://www.linkedin.com/in/nowellgata/" target="_blank">
+    <i class="fab fa-instagram"></i>
+  </a>
+  <a href="https://www.instagram.com/nolerama" target="blank">
+    <i class="fab fa-github-square"></i>
+   </a>
+</footer>
+`;
+
+
+// `<footer class="container flex">
+//   &copy; 2019 Nowell Gata
+//   <ul>
+//     <li>
+//       <a href="https://github.com/NowellGata" target="_blank">
+//         <span class="fab fa-github"></span>
+//       </a>
+//     </li>
+//     <li>
+//       <a href="https://www.linkedin.com/in/nowellgata/" target="_blank">
+//         <span class="fab fa-linkedin-in"></span>
+//       </a>
+//     </li>
+//     <li>
+//       <a href="https://www.instagram.com/nolerama" target="_blank">
+//         <span class="fab fa-instagram"></span>
+//       </a>
+//     </li>
+//   </ul>
+// </footer>`;

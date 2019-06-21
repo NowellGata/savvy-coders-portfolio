81db073f3d9304a0ca4d3876fb03b4cc2f0b7053
Nowell Gata <nowell.gata@gmail.com>
2019-06-21 00:16:48 -0500

    Make CSS look better

--- bricks/pages/Home.js
+++ bricks/pages/Home.js
@@ -8,29 +8,30 @@ export default function(state){
     </h3>
     <div class="nole">
         <img src="https://i.imgur.com/sHc0ckn.png" alt="Avatar" />
+    </div>
 
-
-        <div class="centro">
+    <div class="centro">
             <button class="emailbutton" action="mailto:nowell.gata@gmail.com" style="vertical-align:middle"><span>Email Me! </span></button>
         </div>
 
 
         <div class="howdy">
+         <p>
             There's nothing you could have done, Luke, had you been there. You'd have been killed, too, and the droids would be in the hands of the Empire. I want to come with you to Alderaan. There's nothing here for me now. I want to learn the ways of the Force
-            and become a Jedi like my father. Mos Eisley Spaceport. You will never find a more wretched hive of scum and villainy. We must be cautious. How long have you had these droids? About three or four seasons. They're for sale if you want them. Several
+            and become a Jedi like my father. Mos Eisley Spaceport. You will never find a more wretched hive of scum and villainy. We must be cautious.
+        </p>
+        <p>
+         How long have you had these droids? About three or four seasons. They're for sale if you want them. Several
             fighters have broken off from the main group. Come with me! Pull in! Luke...pull in! Watch your back, Luke! Watch your back! Fighter's above you, coming in! I'm hit, but not bad. Artoo, see what you can do with it. Hang on back there. Red Six...
             Can you see Red Five? There's a heavy fire zone on this side. Red Five, where are you? I can't shake him! I'm on him, Luke! Hold on! Blast it! Wedge where are you? Thanks, Wedge. Good shooting, Wedge!
-        </div>
+        </p>
+            </div>
 
+    <div class="flex-container">
+        <img src="https://i.imgur.com/LyrfoIo.jpg" alt="Restaurant/Menu Development" height="200" width="200">Restaurant Consultation</<img>
+        <img src="https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/903/s300/savvy-coders-logo.png" alt="SavvyCoders" height="200" width="200">Savvy Coders Project</<img>
+        <img src="https://i.imgur.com/yMTD54G.jpg" alt="Savvy Coders Hackathon" height="200" width="200">Creative Work</img>
     </div>
-    <div class="container">
-        <div><img src="https://i.imgur.com/LyrfoIo.jpg" alt="Restaurant/Menu Development" height="200" width="200"></div>
-        <div>Restaurant Consultation</div>
-        <div><img src="https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/903/s300/savvy-coders-logo.png" alt="SavvyCoders" height="200" width="200"></div>
-        <div>Savvy Coders Project</div>
-        <div><img src="https://i.imgur.com/yMTD54G.jpg" alt="Savvy Coders Hackathon" height="200" width="200">
-            <div>Creative Work</div>
-        </div>
 
 `;
 }
